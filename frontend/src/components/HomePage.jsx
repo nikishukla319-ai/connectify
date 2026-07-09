02d37e0b67e9daca8d35cb36db36541a18bac849
nikishukla319-ai <nikishukla319@gmail.com>
2026-07-09 15:17:05 +0530

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Sidebar from './Sidebar'
 import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux';
 
 const HomePage = () =>{
+    const {selectedUser} = useSelector(store=>store.user);
     return(
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <Sidebar/>
-        <MessageContainer/>
-
+    <div className='flex flex-col md:flex-row w-full h-full sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <div className={`${selectedUser ? 'hidden' : 'flex'} md:flex flex-col w-full md:w-auto`}>
+          <Sidebar/>
+        </div>
+        <div className={`${selectedUser ? 'flex' : 'hidden'} md:flex flex-col w-full`}>
+          <MessageContainer/>
+        </div>
     </div>
     )
 }
