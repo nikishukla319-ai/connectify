956ea3096e6cd3e11d4ee0bf285abdc3fbc7d561
nikishukla319-ai <nikishukla319@gmail.com>
2026-07-09 15:45:28 +0530

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -7,10 +7,10 @@ const HomePage = () =>{
     const {selectedUser} = useSelector(store=>store.user);
     return(
     <div className='flex flex-col md:flex-row w-full h-full sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-       <div className={`${selectedUser ? 'hidden' : 'flex'} md:flex flex-col w-full md:w-auto`}>
+        <div className={`${selectedUser ? 'hidden' : 'flex'} md:flex flex-col w-full md:w-auto h-full`}>
           <Sidebar/>
         </div>
-      <div className={`${selectedUser ? 'hidden' : 'flex'} md:flex flex-col w-full md:w-auto`}>
+        <div className={`${selectedUser ? 'flex' : 'hidden'} md:flex flex-col w-full h-full`}>
           <MessageContainer/>
         </div>
     </div>
