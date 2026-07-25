3ff2416ce07419fc2f8475713a62f6ad1e394146
nikishukla319-ai <nikishukla319@gmail.com>
2026-07-25 10:02:20 +0530

    Update userRoute.js

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,13 +1,15 @@
 import express from "express";
-import{register,login,logout, getOtherUsers} from "../controllers/userController.js";
+import{register,login,logout, getOtherUsers, connectRequest, acceptRequest} from "../controllers/userController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
-
+ 
 const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/").get(isAuthenticated,getOtherUsers);
-
-
-
+router.route("/connect/:id").post(isAuthenticated,connectRequest);
+router.route("/accept/:id").post(isAuthenticated,acceptRequest);
+ 
+ 
+ 
 export default router;
